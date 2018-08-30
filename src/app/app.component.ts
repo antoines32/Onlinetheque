ffb69f2db34daad520d254cfdf51658b9b038a78
Antoine Svahn <antoine.svahn@cgi.com>
2018-08-30 18:06:19 +0200

    conf service auth with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Onlinetheque';
+  constructor() {
+    const config = {
+      apiKey: 'AIzaSyDAROtU6JBuouLfc-iI3Dk1l_0dDrBBBRk',
+      authDomain: 'onlinetheque.firebaseapp.com',
+      databaseURL: 'https://onlinetheque.firebaseio.com',
+      projectId: 'onlinetheque',
+      storageBucket: 'onlinetheque.appspot.com',
+      messagingSenderId: '1006143835832'
+    };
+    firebase.initializeApp(config);
+  }
 }
