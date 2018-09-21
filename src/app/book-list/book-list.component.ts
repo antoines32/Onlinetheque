1084ef2dce5961b3f07d5fe0ef8e5311a98e6b67
Antoine Svahn <antoine.svahn@cgi.com>
2018-09-21 15:24:03 +0200

    dev booklist component

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {BookModel} from '../models/book.model';
+import {BooksService} from '../services/books.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
-
-  constructor() { }
+export class BookListComponent implements OnInit, OnDestroy {
+  bookSubscription: Subscription;
+  books: BookModel[];
+  constructor(private bookService: BooksService, private router: Router) { }
 
   ngOnInit() {
+    this.bookSubscription = this.bookService.bookSubject.subscribe(
+      (books: BookModel[]) => { this.books = books; });
+    this.bookService.emitBooks();
+  }
+  onNewBook() {
+    this.router.navigate(['/books', 'new']);
+  }
+  onDelete(book: BookModel) {
+    this.bookService.removeBook(book);
+  }
+  onViewBook(id: number) {
+    this.router.navigate(['/books', 'view']);
+  }
+  ngOnDestroy() {
+    this.bookSubscription.unsubscribe();
   }
 
 }
